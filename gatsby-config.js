0c2492eaa71ada38ffd5fb10a7201b4719fcf15c
leeye51456 <leeye51456@gmail.com>
2021-03-24 23:47:34 +0900

    Update `gatsby-config.js`

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `&#51456;`,
+    description: `A blog of leeye51456`,
+    author: `leeye51456`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `&#51456;`,
+        short_name: `&#51456;`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
