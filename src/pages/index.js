4cc7e64813d649b931094c713a3509c81209b8b8
leeye51456 <leeye51456@gmail.com>
2021-03-26 00:19:57 +0900

    Add post data to the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,46 @@
-import * as React from 'react';
-import { Link } from 'gatsby';
-import { StaticImage } from 'gatsby-plugin-image';
+import React from 'react';
+import { graphql, Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={['AUTO', 'WEBP', 'AVIF']}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-);
+const IndexPage = ({ data }) => {
+  const posts = data.allMarkdownRemark.edges.map(({ node }) => (
+    <Link key={node.id} to={node.frontmatter.slug}>
+      <div>
+        <h1>{node.frontmatter.title}</h1>
+        <div>{node.frontmatter.date}</div>
+        <div>{node.excerpt}</div>
+      </div>
+    </Link>
+  ));
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>전체 게시물 {data.allMarkdownRemark.totalCount}개</div>
+      <div>{posts}</div>
+    </Layout>
+  )
+};
 
 export default IndexPage;
+
+export const query = graphql`
+  {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "YYYY-MM-DD")
+            slug
+          }
+          excerpt(truncate: true)
+        }
+      }
+    }
+  }
+`;
