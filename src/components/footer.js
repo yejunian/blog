76717eb80087adace631808e9e45ace7d7f84db3
leeye51456 <leeye51456@gmail.com>
2021-04-01 22:59:20 +0900

    Simplify Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,28 +6,25 @@ const years = (() => {
   return `${year > 2021 ? '2021-' : ''}${year}`;
 })();
 
-const Footer = () => {
-  console.log(years);
-  return (
-    <footer>
-      <ul>
-        <li>
-          <a href="https://leeye51456.github.io/">Home</a>
-        </li>
-        <li>
-          <Link to="/">Blog</Link> (
-          <a href="https://github.com/leeye51456/blog">Repository</a>)
-        </li>
-        <li>
-          <a href="https://leeye51456.github.io/portfolio/">Portfolio</a>
-        </li>
-      </ul>
-      <p>
-        © {years} leeye51456, Built with{' '}
-        <a href="https://www.gatsbyjs.com/">Gatsby</a>
-      </p>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer>
+    <ul>
+      <li>
+        <a href="https://leeye51456.github.io/">Home</a>
+      </li>
+      <li>
+        <Link to="/">Blog</Link> (
+        <a href="https://github.com/leeye51456/blog">Repository</a>)
+      </li>
+      <li>
+        <a href="https://leeye51456.github.io/portfolio/">Portfolio</a>
+      </li>
+    </ul>
+    <p>
+      © {years} leeye51456, Built with{' '}
+      <a href="https://www.gatsbyjs.com/">Gatsby</a>
+    </p>
+  </footer>
+);
 
 export default Footer;
