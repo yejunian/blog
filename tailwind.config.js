f9635ec010e0386a902123d9274de3b8b2a9b0d7
leeye51456 <leeye51456@gmail.com>
2021-04-04 00:09:38 +0900

    Adjust line height presets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      lineHeight: {
+        tight: '1.2',
+        snug: '1.4',
+        normal: '1.6',
+        relaxed: '1.8',
+        loose: '2',
+      },
+    },
   },
   variants: {
     extend: {},
