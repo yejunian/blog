5128cf25bdfe0ed0b6f1f207e2a9607078aeba16
leeye51456 <leeye51456@gmail.com>
2021-04-10 00:10:07 +0900

    Change max width

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      md: '640px',
+      lg: '1280px',
+    },
     extend: {
       lineHeight: {
         tight: '1.2',
