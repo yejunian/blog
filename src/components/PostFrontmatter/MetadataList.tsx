805de18d2b8b0c892c2512aaaf9c2e931acbdc24
yejunian <leeye51456@gmail.com>
2022-10-09 23:49:34 +0900

    Chore(PostFrontmatter): Remove curly brackets

--- src/components/PostFrontmatter/MetadataList.tsx
+++ src/components/PostFrontmatter/MetadataList.tsx
@@ -11,36 +11,34 @@ export type PostMetadataItem = {
   values?: string[] | string | null
 }
 
-const MetadataList = ({ metadata }: PostRestFrontmatterProps) => {
-  return (
-    <dl className={styles.root}>
-      {metadata.reduce<React.ReactNode[]>((acc, { key, values }) => {
-        if (!values || values.length === 0) {
-          return acc
-        }
-
-        let valueArray: string[]
-        if (typeof values === 'string') {
-          valueArray = [values]
-        } else {
-          valueArray = values
-        }
-
-        acc.push(
-          <div key={key} className={styles.pair}>
-            <dt className={styles.key}>{key}</dt>
-            {valueArray.map((value) => (
-              <dd key={value} className={styles.value}>
-                {value}
-              </dd>
-            ))}
-          </div>
-        )
-
+const MetadataList = ({ metadata }: PostRestFrontmatterProps) => (
+  <dl className={styles.root}>
+    {metadata.reduce<React.ReactNode[]>((acc, { key, values }) => {
+      if (!values || values.length === 0) {
         return acc
-      }, [])}
-    </dl>
-  )
-}
+      }
+
+      let valueArray: string[]
+      if (typeof values === 'string') {
+        valueArray = [values]
+      } else {
+        valueArray = values
+      }
+
+      acc.push(
+        <div key={key} className={styles.pair}>
+          <dt className={styles.key}>{key}</dt>
+          {valueArray.map((value) => (
+            <dd key={value} className={styles.value}>
+              {value}
+            </dd>
+          ))}
+        </div>
+      )
+
+      return acc
+    }, [])}
+  </dl>
+)
 
 export default MetadataList
--- src/components/PostFrontmatter/index.tsx
+++ src/components/PostFrontmatter/index.tsx
@@ -17,29 +17,27 @@ const PostFrontmatter = ({
   subtitle,
   description,
   rest,
-}: PostFrontmatterProps) => {
-  return (
-    <div className={styles.root}>
-      <img
-        className={styles.thumbnail}
-        role="presentation"
-        src={thumbnail}
-        width={1200}
-        height={630}
-      />
+}: PostFrontmatterProps) => (
+  <div className={styles.root}>
+    <img
+      className={styles.thumbnail}
+      role="presentation"
+      src={thumbnail}
+      width={1200}
+      height={630}
+    />
 
-      <div className={styles.text}>
-        <header className={styles.titleGroup}>
-          <h1 className={styles.title}>{title}</h1>
-          {subtitle && <p className={styles.subtitle}>&mdash; {subtitle}</p>}
-        </header>
+    <div className={styles.text}>
+      <header className={styles.titleGroup}>
+        <h1 className={styles.title}>{title}</h1>
+        {subtitle && <p className={styles.subtitle}>&mdash; {subtitle}</p>}
+      </header>
 
-        <p className={styles.description}>{description}</p>
+      <p className={styles.description}>{description}</p>
 
-        <MetadataList metadata={rest} />
-      </div>
+      <MetadataList metadata={rest} />
     </div>
-  )
-}
+  </div>
+)
 
 export default PostFrontmatter
