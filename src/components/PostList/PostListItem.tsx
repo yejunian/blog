8f76b55647e5e0b111d575a54b0c7e50357a25f0
yejunian <leeye51456@gmail.com>
2022-10-21 18:17:52 +0900

    Feat(PostList): Implement components

---
+++ src/components/PostList/PostListItem.tsx
@@ -0,0 +1,74 @@
+import { Link } from 'gatsby'
+import { GatsbyImage, IGatsbyImageData, StaticImage } from 'gatsby-plugin-image'
+import React, { useRef } from 'react'
+
+import * as styles from './PostListItem.module.scss'
+
+export type PostListItemProps = {
+  path?: string | null
+  date?: string | null
+  title?: string | null
+  subtitle?: string | null
+  description?: string | null
+  keywords?: string[] | null
+  thumbnail?: IGatsbyImageData | null
+  thumbnailAlt?: string | null
+}
+
+const PostListItem = ({
+  path,
+  date,
+  title,
+  subtitle,
+  description,
+  thumbnail,
+  thumbnailAlt,
+  keywords = [],
+}: PostListItemProps) => {
+  const linkRef = useRef<Link<unknown> & HTMLAnchorElement>(null)
+
+  const handleRootClick: React.MouseEventHandler = (event) => {
+    if (event.target !== linkRef.current) {
+      linkRef.current?.click()
+    }
+  }
+
+  return (
+    <article className={styles.root} onClick={handleRootClick}>
+      <div className={styles.text}>
+        <h3 className={styles.title}>
+          <Link ref={linkRef} to={path ?? '/404'}>
+            {path ? null : '[대상 페이지 접근 불가] '}
+            {title || '(제목 정보 없음)'}
+            {subtitle ? ` \u2015 ${subtitle}` : null}
+          </Link>
+        </h3>
+        {description && <div className={styles.description}>{description}</div>}
+        <div className={styles.metadata}>
+          <div>{date || '(작성 날짜 정보 없음)'}</div>
+          <div className={styles.keywords}>{keywords?.join(', ')}</div>
+        </div>
+      </div>
+
+      <div className={styles.thumbnail}>
+        {thumbnail ? (
+          <GatsbyImage
+            className={styles.imageRoot}
+            image={thumbnail}
+            alt={thumbnailAlt ?? ''}
+          />
+        ) : (
+          <StaticImage
+            className={styles.imageRoot}
+            src="../images/thumbnail-fallback.png"
+            alt=""
+            layout="fullWidth"
+            breakpoints={[216, 432]}
+          />
+        )}
+      </div>
+    </article>
+  )
+}
+
+export default PostListItem
---
+++ src/components/PostList/index.tsx
@@ -0,0 +1,56 @@
+import React, { useMemo } from 'react'
+
+import categoryMetadata from '../../blog-post/src/categoryMetadata'
+
+import * as styles from './PostList.module.scss'
+import PostListItem, { PostListItemProps } from './PostListItem'
+
+type PostListProps = {
+  heading?: string
+  categoryId: string
+  items: PostListItemArray
+}
+
+// TODO - Combine with PostMetadataItem type
+export type PostListItemArray = PostListItemArrayEntry[]
+export type PostListItemArrayEntry = PostListItemProps & { id: string }
+
+const PostList = ({ heading, categoryId, items }: PostListProps) => {
+  const categoryLabel = categoryMetadata.get(categoryId)?.label
+  const fallbackHeadingPrefix =
+    !heading && categoryLabel ? `${categoryLabel}에는 ` : ''
+
+  const postListItems = useMemo(
+    () =>
+      items.map((item) => (
+        <PostListItem
+          key={item.id}
+          path={item.path}
+          date={item.date}
+          title={item.title}
+          subtitle={item.subtitle}
+          description={item.description}
+          keywords={item.keywords}
+          thumbnail={item.thumbnail}
+          thumbnailAlt={item.thumbnailAlt}
+        />
+      )),
+    [items]
+  )
+
+  return (
+    <div className={styles.root}>
+      <h2 className={styles.title}>
+        <a href="#">{heading ?? `${fallbackHeadingPrefix}무엇을 끄적였나`}</a>
+      </h2>
+
+      <div className={styles.list}>{postListItems}</div>
+
+      <div className={styles.more}>
+        <a href="#">‘{categoryLabel}’ 카테고리의 모든 글 보기 &gt;</a>
+      </div>
+    </div>
+  )
+}
+
+export default PostList
