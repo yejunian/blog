64526a44458bcd8a0381b1cf3a722c5409e81495
yejunian <leeye51456@gmail.com>
2025-10-15 14:06:59 +0900

    📦️ deps: Astro 업그레이드, 통합 설치(React, MDX)
    이 작업을 되돌리려면 `tsconfig.json`, `astro.config.mjs`, `package.json`
    파일의 변화를 참고합니다. React, MDX, JSX 관련 내용에 변경사항이
    있습니다.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://yejunian.github.io",
   base: "/blog/",
+
+  integrations: [react(), mdx()],
 });
